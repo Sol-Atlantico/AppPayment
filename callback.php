b51f59af5ea89c30f9212b8a3baf6845f2ae8000
Denilson Tavares <dstavares00@gmail.com>
2022-10-06 17:57:06 -0100

    error messages

--- callback.php
+++ callback.php
@@ -32,14 +32,15 @@ if(isset($_POST["messageType"]) && in_array($_POST["messageType"], $successMessa
 	if($_POST["resultFingerPrint"] == $fingerPrintCalculado)
 	{
 		// TRANSACAO BEM SUCEDIDA
-		echo "<h1>Sucesso</h1>";
+		echo "<img src="sucesso.svg" width="500" height="600">"
+		echo "<h1>Pagamento Realizado Com Sucesso</h1>";
 
 	}
 	else
 	{
 		// FINGER PRINT RECEBIDO E CALCULADO SAO DIFERENTES
-
-		echo "<h1>Erro</h1>";
+		echo "<img src="error.svg" width="500" height="600">"
+		echo "<h1>Pagamento Inválido</h1>";
 		echo "<p>Finger Print de Resposta Inválida</p>";
 	}
 }
@@ -49,7 +50,7 @@ else if(isset($_POST["UserCancelled"]) && $_POST["UserCancelled"] == "true")
 }
 else
 {
-	echo "<h1>Erro</h1>";
+	echo "<h1>Pagamento Inválido</h1>";
 
 	echo "<br>";
 	echo "<p>" . $_POST["merchantRespErrorDescription"] . "</p>";
@@ -71,31 +72,6 @@ else
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
-	
-	<div class="container">
-		<h3 class="my-4">Resposta da Transacção | em PHP</h3>
-
-			<table class="table">
-				<tr>
-					<th>Key</th>
-					<th>Value</th>
-				</tr>
-				<?php
-					
-					foreach($_POST as $key => $value)
-					{
-						echo "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
-					}
-
-					foreach($_GET as $key => $value)
-					{
-						echo "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
-					}
-
-				?>				
-			</table>
-
-	</div>
 
 </body>
 </html>
